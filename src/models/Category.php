d101983be0283e40dd5e657f46e506e744da5e12
Jensone <hello@jensone.com>
2023-12-20 16:03:37 +0100

    🐛 corriger(Category.php) : supprimer la valeur par défaut de la propriété protégée $image ✨ fonctionnalité(Category.php) : ajouter la propriété protégée $image à la liste des champs et des valeurs bindées

--- src/models/Category.php
+++ src/models/Category.php
@@ -8,9 +8,9 @@ class Category extends AbstractModel
 {
     private int $id;
     private string $name;
-    protected string $image = ':image';
+    private string $image;
     protected string $table = 'categories';
-    protected string $fields = 'name';
+    protected string $fields = 'name, imageoco';
     protected string $values = ':name, :image';
     protected array $valuesBinded = [
         ':name' => '',
